4895e2e5cac68ccc03b5b87741a441872d8edf60
Super Hornets <51898694+SuperHornetsTeam@users.noreply.github.com>
2023-01-21 14:52:31 -0600

    Added PickerUpper class and stubs.

---
+++ src/main/java/frc/robot/PickerUpper.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+public class PickerUpper {
+    public void up(double angle) {}
+    public void down(double angle) {}
+
+    public void left(double angle) {}
+    public void right(double angle) {}
+    
+    public void extend(double speed) {}
+    public void retract(double speed) {}
+
+    public void open(double angle) {}
+    public void closeCone() {}
+    public void closeCube() {}
+}
+
