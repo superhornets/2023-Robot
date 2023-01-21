b3f64b6b535db7b79bf414c340a143ef96635116
Super Hornets <51898694+SuperHornetsTeam@users.noreply.github.com>
2023-01-21 15:17:32 -0600

    Added drive class with stub functions.

---
+++ src/main/java/frc/robot/Drive.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class Drive {
+
+    public void driveTo(double distance) {}
+
+    public void turnTo(double angle) {}
+
+    public void arcade(double forwardSpeed, double turnSpeed) {}
+
+    public void level() {}
+}
