df33d223916561f9a21ac5fcdd8e454000bfef50
Felarof1 <98996513+Felarof1@users.noreply.github.com>
2023-07-13 19:17:44 -0500

    changed grabber open speed

--- src/main/java/frc/robot/Grabber.java
+++ src/main/java/frc/robot/Grabber.java
@@ -71,7 +71,7 @@ public class Grabber extends SubsystemBase {
     if (false) {
       m_grabber.set(0);
     } else {
-      m_grabber.set(grabberSpeed);
+      m_grabber.set(.4);
     }
     // }
 
